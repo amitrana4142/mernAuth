6a3c636338daf897d2d048d9daf18eac780a2c37
unknown <amitrana4142@gmail.com>
2024-02-10 23:22:31 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+    <h3 className='text-green-500 text-3xl font-bold  ' >Hello tailwind</h3>
+    
+    </>
+  )
+}
